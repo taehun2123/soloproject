0836ff800364a79ed297814ea0845b415cf0777c
taehun2123 <pyoneng_@naver.com>
2023-11-07 17:33:41 +0900

    Fix : value값을 잘못 지정 했던 점 수정

--- blog/src/store/store.js
+++ blog/src/store/store.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 export const usePostStore = create((set)=>({
   hlist : "",
-  setHlist : (val) => set( (state) => ({ hlist : state }) ),
+  setHlist : (val) => set( (state) => ({ hlist : val }) ),
   plist : "",
-  setPlist : (val) => set( (state) => ({ hlist : state }) )
+  setPlist : (val) => set( (state) => ({ plist : val }) )
 }))
